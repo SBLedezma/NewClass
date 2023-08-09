99f20c30da9061b00a5269b54cbd3729f294eedb
SBLedezma <141078486+SBLedezma@users.noreply.github.com>
2023-08-09 23:57:27 +0000

    Clase 5

--- main.py
+++ main.py
@@ -63,9 +63,31 @@ i=int(input("cuanto interes anual mano "))
 A=int(input("y cuantos años mi rey "))
 Formula=round(I*(i/100+1)**A)
 print("esto hicimos mi rey",Formula)
-'''
+
 #ejercicio 10
 P=int(input("cuantos payasos va a llevar mi rey "))
 M=int(input("y cuantas Muñecas va a llevar mi rey "))
 R=(P*112)+(M*75)
 print("el paquete pesa ",R)
+
+#ejercicio 11
+#ejercicio 12
+
+#ejercicio 13
+N=int(input("cuantas puñaladas le pegaron "))
+R=N*(N+1)/2
+if R>20:
+  print(R,"usted esta mueto mi hermano")
+else:
+  print(R,"al menos esta vivo")
+  '''
+#ejercicio 14
+N=int(input("dame un numero entero sapa "))
+M=int(input("Dame otro numero sapa "))
+C=N//M
+D=N/M
+R=N%M
+print("el resultado de la divicion es ",D," El cociente es ",C," y el resto es ",R)
+if C<1:
+  print("El divisor es mayor al dividiendo")
+  
